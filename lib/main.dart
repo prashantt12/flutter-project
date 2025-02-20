fca37c5069b822368e4a731829433f0fd47229d9
prashantt12 <89059580+prashantt12@users.noreply.github.com>
2025-02-21 00:37:23 +0530

    added image for login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_first_version/pages/homepage.dart';
 import 'package:flutter_first_version/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -13,12 +14,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // adjusting the theme mode as dark/light according to the system settings
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          // adding primary font as some font from the google-font library
+          fontFamily: GoogleFonts.acme().fontFamily,
           appBarTheme: AppBarTheme(backgroundColor: Colors.deepPurple, foregroundColor: Colors.white)),
       darkTheme: ThemeData(brightness: Brightness.dark),
 
-      // defining the inital route for the application
-      initialRoute: "/home",
+      // defining the initial-route for the application
+      initialRoute: "/",
+
       // defining the routes for the application
       routes: {
         // defining the initial route for the app
